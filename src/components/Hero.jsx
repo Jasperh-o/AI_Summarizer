54b4a12cc9994ff992446d54a7c391b4065d88ee
Jasperh-o <101496955+Jasperh-o@users.noreply.github.com>
2024-04-09 18:17:17 +0300

    1st commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+import { logo } from '../assets/assets';
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col"></header>
+  );
+};
+
+export default Hero;
